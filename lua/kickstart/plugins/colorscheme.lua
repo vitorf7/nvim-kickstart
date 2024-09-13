148c714920859b3de7f78a6eaf0eb9b3c648ba5a
Vitor Faiante <vfaiante@uw.co.uk>
2024-09-13 13:29:28 +0100

    refactor: add imports to new themes

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 -- local theme = require 'kickstart.plugins.themes.tokyonight'
 -- local theme = require 'kickstart.plugins.themes.catppuccin'
 -- local theme = require 'kickstart.plugins.themes.fluoromachine'
-local theme = require 'kickstart.plugins.themes.cyberdream'
+local theme = require 'kickstart.plugins.themes.citruszest'
+-- local theme = require 'kickstart.plugins.themes.rosepine'
+-- local theme = require 'kickstart.plugins.themes.cyberdream'
 
 return theme
