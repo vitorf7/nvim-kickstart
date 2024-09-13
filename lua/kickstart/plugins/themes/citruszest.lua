1af98ce80f9e41bac3990fb7c67623f68eebf0bf
Vitor Faiante <vfaiante@uw.co.uk>
2024-09-13 13:28:16 +0100

    feat: add citruszest and rosepine themes

---
+++ lua/kickstart/plugins/themes/citruszest.lua
@@ -0,0 +1,13 @@
+return {
+  'zootedb0t/citruszest.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('citruszest').setup {
+      option = {
+        transparent = true,
+      },
+    }
+    vim.cmd.colorscheme 'citruszest'
+  end,
+}
