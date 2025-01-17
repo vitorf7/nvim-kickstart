fc9964b7f5e2a83c63f418ae07e57d12b5c732d2
Vitor Faiante <vfaiante@uw.co.uk>
2025-01-17 15:01:36 +0000

    chore: try to remove telescope from dropbar and neogit

--- lua/kickstart/plugins/dropbar.lua
+++ lua/kickstart/plugins/dropbar.lua
@@ -1,9 +1,7 @@
 return {
   'Bekaboo/dropbar.nvim',
   -- optional, but required for fuzzy finder support
-  dependencies = {
-    -- "nvim-telescope/telescope-fzf-native.nvim",
-  },
+  dependencies = {},
   config = function()
     vim.cmd [[hi WinBar guibg=NONE]]
   end,
--- lua/kickstart/plugins/neogit.lua
+++ lua/kickstart/plugins/neogit.lua
@@ -4,11 +4,25 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
-
-    -- Only one of these is needed, not both.
-    'nvim-telescope/telescope.nvim', -- optional
   },
-  config = true,
+  config = function()
+    require('neogit').setup {
+      integrations = {
+        -- If enabled, use telescope for menu selection rather than vim.ui.select.
+        -- Allows multi-select and some things that vim.ui.select doesn't.
+        telescope = false,
+        -- If enabled, uses fzf-lua for menu selection. If the telescope integration
+        -- is also selected then telescope is used instead
+        -- Requires you to have `ibhagwan/fzf-lua` installed.
+        fzf_lua = false,
+
+        -- If enabled, uses mini.pick for menu selection. If the telescope integration
+        -- is also selected then telescope is used instead
+        -- Requires you to have `echasnovski/mini.pick` installed.
+        mini_pick = false,
+      },
+    }
+  end,
   keys = {
     { '<leader>gG', '<cmd>Neogit<cr>', desc = 'Neogit' },
   },
