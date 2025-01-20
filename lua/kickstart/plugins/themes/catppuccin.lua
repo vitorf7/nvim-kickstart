bd2c6f365a07bd87977ec8cb4e50ce5fb0ba7c24
Vitor Faiante <vfaiante@uw.co.uk>
2025-01-20 20:30:37 +0000

    feat: add rose pine

--- lua/kickstart/plugins/themes/catppuccin.lua
+++ lua/kickstart/plugins/themes/catppuccin.lua
@@ -1,78 +1,10 @@
--- local M = {
---   "catppuccin/nvim",
---   lazy = false,
---   name = "catppuccin",
---   priority = 1000,
---   ---@class CatppuccinOptions
---   opts = {
---     flavour = "mocha",
---     transparent_background = true,
---     integrations = {
---       alpha = true,
---       cmp = true,
---       dap = true,
---       fidget = true,
---       flash = true,
---       gitsigns = true,
---       harpoon = true,
---       lsp_trouble = true,
---       mason = true,
---       neotree = true,
---       neotest = true,
---       noice = true,
---       navic = {
---         enabled = true,
---         custom_bg = "NONE", -- "lualine" will set background to mantle
---       },
---       notify = true,
---       octo = true,
---       overseer = true,
---       telescope = {
---         enabled = true,
---       },
---       treesitter = true,
---       treesitter_context = false,
---       symbols_outline = true,
---       illuminate = true,
---       which_key = true,
---       barbecue = {
---         dim_dirname = true,
---         bold_basename = true,
---         dim_context = false,
---         alt_background = false,
---       },
---       native_lsp = {
---         enabled = true,
---         virtual_text = {
---           errors = { "italic" },
---           hints = { "italic" },
---           warnings = { "italic" },
---           information = { "italic" },
---         },
---         underlines = {
---           errors = { "underline" },
---           hints = { "underline" },
---           warnings = { "underline" },
---           information = { "underline" },
---         },
---       },
---     },
---   },
---   config = function(_, opts)
---     require("catppuccin").setup(opts)
---     require("catppuccin").load()
---
---     vim.cmd.colorscheme("catppuccin")
---   end,
--- }
-
 local M = {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 2000,
   ---@class CatppuccinOptions
-  opts = function()
-    return {
+  config = function()
+    require('catppuccin').setup {
       flavour = 'mocha',
       transparent_background = true,
       -- custom_highlights = function(colors)
@@ -125,6 +57,7 @@ local M = {
         },
       },
     }
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
 
