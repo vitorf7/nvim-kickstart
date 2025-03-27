2e8c5a819d94ecfeafc77f07656f077ea8086928
Vitor Faiante <vfaiante@uw.co.uk>
2025-03-27 19:22:01 +0000

    feat: move to lualine

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -28,19 +28,19 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    statusline.setup()
-
-    -- You can confiure sections in the statusline by overriding their
-    -- default behavior. For example, here we disable the section for
-    -- cursor information because line numbers are already enabled
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+    -- -- Simple and easy statusline.
+    -- --  You could remove this setup call if you don't like it,
+    -- --  and try some other statusline plugin
+    -- local statusline = require 'mini.statusline'
+    -- statusline.setup()
+    --
+    -- -- You can confiure sections in the statusline by overriding their
+    -- -- default behavior. For example, here we disable the section for
+    -- -- cursor information because line numbers are already enabled
+    -- ---@diagnostic disable-next-line: duplicate-set-field
+    -- statusline.section_location = function()
+    --   return '%2l:%-2v'
+    -- end
 
     local mini_pairs = require 'mini.pairs'
     mini_pairs.setup()
